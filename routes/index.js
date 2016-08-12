940ab353fcb48a4f55306d76ead00ebbf1b5e39c
andrew franklin <theandrewfranklin@gmail.com>
2016-08-12 12:37:10 -0700

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  res.send('<iframe src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyD_yzTWnGjID6IUWj9PF9IVhIFwYtCp_fM&origin=Oslo+Norway&destination=Telemark+Norway&avoid=tolls|highways" width="600" height="450"></iframe>')
+})
+
 module.exports = router;
