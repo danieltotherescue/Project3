5879ec2da4d2429f71155df1bb819b7380779b88
andrew franklin <theandrewfranklin@gmail.com>
2016-08-14 16:01:24 -0700

    submits to mongo

--- models/Search.js
+++ models/Search.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 
-var Search = mongoose.Schema){
+var Search = new mongoose.Schema({
   starting_point: {type: String, required: true},
-  destination: {type: String, required: true}
-}
+  destination: {type: String, required: true},
+  car: {make: String, model: String, Year: Date}
+})
 
 module.exports = mongoose.model('Search', Search)
