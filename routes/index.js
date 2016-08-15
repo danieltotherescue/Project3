bffd91ad0aeb2399fba9e3d1433e938ac0e4dd58
andrew franklin <theandrewfranklin@gmail.com>
2016-08-15 11:21:06 -0700

    fixed mongo conflict

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.post('/', function(req, res, next) {
   var destination = req.body.destination;
   var startingLoc = req.body.startingLoc;
   res.send('<iframe width="70%" height="70%" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyD_yzTWnGjID6IUWj9PF9IVhIFwYtCp_fM&origin=' + startingLoc + '&destination=' + destination + '"></iframe>')
+})
 
 router.get('/auth/google', passport.authenticate(
   'google',
