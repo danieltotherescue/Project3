0836624222e9319aa1429a252a391b8113d422d1
Michael.s.Budde@gmail.com </ignore>
2016-08-16 11:15:17 -0700

    storing searches in database

--- config/routes.js
+++ config/routes.js
@@ -4,9 +4,16 @@ var bodyParser = require('body-parser');
 var request    = require('request')
 //Controller
 var pagesController = require('../controllers/pages')
+var searchController = require('../controllers/search')
 
 router.route('/')
   .get(pagesController.index);
 
+
+  // api search routes
+
+router.route('/api/search')
+  .get(searchController.index)
+  .post(searchController.create);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express  = require('express');
 var router   = express.Router();
 var passport = require('passport');
+var searchController = require('../controllers/search');
 
 
+router.route('/api/search')
+  .get(searchController.index)
+  .post(searchController.create);
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Roadtrippr', user: req.user });
 });
 
-router.get('/about', function(req, res, next){
+router.get('/about', function(req, res, next) {
   res.render('pages/about', { title: 'About Roadtrippr', user: req.user });
 });
 
@@ -17,6 +21,16 @@ router.post('/', function(req, res, next) {
   var destination = req.body.destination;
   var startingLoc = req.body.startingLoc;
   var hiddenLocation = req.body.hiddenLocation;
+  var Search = require('../models/Search');
+
+    console.log('storing a new search!');
+    var newSearch = new Search();
+    newSearch.starting_point = startingLoc;
+    newSearch.destination = destination;
+    console.log(newSearch);
+    newSearch.save(function(err, savedSearch) {
+      if(err) next (err);
+    });
   if (startingLoc) {
     res.send('<iframe width="70%" height="70%" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyD_yzTWnGjID6IUWj9PF9IVhIFwYtCp_fM&origin=' + startingLoc + '&destination=' + destination + '"></iframe>')
   } else {
