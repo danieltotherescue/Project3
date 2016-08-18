d8a56a2e806e022412b2abd0174d0480ea893ee7
MSBudde <michael.s.budde@gmail.com>
2016-08-18 15:29:39 -0700

    commented on code

--- public/javascript/googlemaps.js
+++ public/javascript/googlemaps.js
@@ -117,17 +117,17 @@ $.when(geoLocation.getLocation()).then(function(data){
           break;
         }
       }
-
-/////////// This section is all about the weather api
+      // finds wind direction and picks correct key symbol for display
       function findWindDirection(dir) {
         var key    = ['N','S','W','E','NW','NE','SW','SE'];
         var select = Math.floor(dir/45);
         return key[select];
       }
 
+      // Grab data from forecast.io using longitude and latitude to grab correct insertBefore
       var apiKey = '76a0b6639f56ec09d980a92471acf6cb';
       var apiURL = 'https://api.forecast.io/forecast/' + apiKey + '/' + $dLatitude + ',' + $dLongitude;
-
+      // Gets data and appends it to destination weather conditions
       $.ajax({
         url: apiURL,
         dataType: 'jsonp',
@@ -168,7 +168,7 @@ $.when(geoLocation.getLocation()).then(function(data){
 
       var apiKey = '76a0b6639f56ec09d980a92471acf6cb';
       var apiURL = 'https://api.forecast.io/forecast/' + apiKey + '/' + latLng.lat + ',' + latLng.lng;
-
+      // Grabs data for midpoint weather conditions
       $.ajax({
         url: apiURL,
         dataType: 'jsonp',
@@ -212,7 +212,7 @@ $.when(geoLocation.getLocation()).then(function(data){
       } else {
         var apiURL = 'https://api.forecast.io/forecast/' + apiKey + '/' + pos.lat + ',' + pos.lng;
           }
-
+          // Grab data for starting location weather conditions
       $.ajax({
         url: apiURL,
         dataType: 'jsonp',
@@ -228,7 +228,7 @@ $.when(geoLocation.getLocation()).then(function(data){
           if(windSpeed > 0) {
             $('#Swind').append('Wind ' + windDirect + '@ ' + windSpeed + ' MPH');
 
-
+            // picks correct icon to display based on data grabbed from api
             var icons = new Skycons({
               'color': 'black'
             }),
